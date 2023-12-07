d6533680065a515052915c3a99c6152362427135
derekdkim <derek.kim@jcatechnologies.com>
2023-12-07 10:37:15 -0500

    Cleanup

--- 2023/7/main.py
+++ 2023/7/main.py
@@ -26,6 +26,8 @@ def parse_hands(lines, p2=False):
 def get_best_hand(hand, p2=False):
     hand_cp = sorted(hand)
     freq = dict()
+    uniques = len(set(hand))
+    hand_size = len(hand)
 
     for card in hand_cp:
         if card in freq:
@@ -33,40 +35,34 @@ def get_best_hand(hand, p2=False):
         else:
             freq[card] = 1
 
-    if 'J' not in hand or not p2:
+    if 'J' not in freq or not p2:
         # Five of a kind - 7
-        if len(set(hand)) == 1:
+        if uniques == 1:
             return 7
-        
         # High Card - 1
-        if len(set(hand)) == len(hand):
+        if uniques == hand_size:
             return 1
-        
         # One Pair - 2
-        if len(set(hand)) == len(hand) - 1:
+        if uniques == hand_size - 1:
             return 2
-        
-        if len(freq) == 2:
+        if hand_size == 2:
             # Four of a kind - 6
             if any(x == 4 for x in freq.values()):
                 return 6
-
             # Full House - 5
             return 5
-        
-        if len(freq) == 3:
+        if hand_size == 3:
             # Three of a kind - 4
             if any(x == 3 for x in freq.values()):
                 return 4
-            
             # Two Pair - 3
             return 3
     else:
-        if len(set(hand)) < 3:
+        if uniques < 3:
             # Five of a kind - 7
             # Legit or with joker
             return 7
-        if len(set(hand)) == 3:
+        if uniques == 3:
             # Four of a kind - 6
             # Full House - 5
             # Legit or with joker
@@ -79,14 +75,12 @@ def get_best_hand(hand, p2=False):
                 return 6
             else:
                 return 5
-        if len(set(hand)) == 4:
+        if uniques == 4:
             # Three of a kind - 4
             # ABCJJ
             # AABCJ
             # Impossible to get 2 pair
-            if freq['J'] > 1 or any(x > 1 for x in freq.values()):
-                return 4
-        
+            return 4
         # Left over - 5 unique cards
         # Form a pair with joker
         return 2
